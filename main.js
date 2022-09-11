2f0c1c964bb9cca8ecf06530e536d74fa581ff87
RaoulBock <anthonyraoulbock01@gmail.com>
2022-09-11 18:20:52 +0200

    road

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(3), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 car.draw(ctx);
 
 animate();
@@ -12,7 +12,10 @@ animate();
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+  ctx.translate(0, -car.y + canvas.height * 0.7);
   road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
   requestAnimationFrame(animate);
 }
