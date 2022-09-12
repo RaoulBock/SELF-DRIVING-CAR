25ab17c253a52d1be30224133c27cbe1bc4b1615
RaoulBock <anthonyraoulbock01@gmail.com>
2022-09-12 21:36:02 +0200

    sensor

--- sensor.js
+++ sensor.js
@@ -1,9 +1,9 @@
 class Sensor {
   constructor(car) {
     this.car = car;
-    this.rayCount = 3;
-    this.rayLength = 100;
-    this.raySpread = Math.PI / 4;
+    this.rayCount = 5;
+    this.rayLength = 150;
+    this.raySpread = Math.PI / 2;
 
     this.rays = [];
     this.readings = [];
@@ -14,6 +14,30 @@ class Sensor {
     this.readings = [];
     for (let i = 0; i < this.rays.length; i++) {
       this.readings.push(this.#getReading(this.rays[i], roadBorders));
+    }
+  }
+
+  #getReading(ray, roadBorders) {
+    let touches = [];
+
+    for (let i = 0; i < roadBorders.length; i++) {
+      const touch = getIntersection(
+        ray[0],
+        ray[1],
+        roadBorders[i][0],
+        roadBorders[i][1]
+      );
+      if (touch) {
+        touches.push(touch);
+      }
+    }
+
+    if (touches.length == 0) {
+      return null;
+    } else {
+      const offsets = touches.map((e) => e.offset);
+      const minOffset = Math.min(...offsets);
+      return touches.find((e) => e.offset == minOffset);
     }
   }
 
@@ -35,16 +59,27 @@ class Sensor {
       this.rays.push([start, end]);
     }
   }
-  #getReading(rays, roadBorders) {}
 
   draw(ctx) {
     try {
       for (let i = 0; i < this.rayCount; i++) {
+        let end = this.rays[i][1];
+        if (this.readings[i]) {
+          end = this.readings[i];
+        }
+
         ctx.beginPath();
         ctx.lineWidth = 2;
         ctx.strokeStyle = "yellow";
         ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
-        ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "black";
+        ctx.moveTo(this.rays[i][1].x, this.rays[i][1].y);
+        ctx.lineTo(end.x, end.y);
         ctx.stroke();
       }
     } catch (e) {
