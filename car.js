746efd204e4436df6c41902cc56c4e8672b06de9
RaoulBock <anthonyraoulbock01@gmail.com>
2022-09-13 22:53:37 +0200

    Parallelization

--- car.js
+++ car.js
@@ -120,7 +120,7 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor = false) {
     if (this.damaged) {
       ctx.fillStyle = "gray";
     } else {
@@ -133,7 +133,7 @@ class Car {
     }
     ctx.fill();
 
-    if (this.sensor) {
+    if (this.sensor && drawSensor) {
       this.sensor.draw(ctx);
     }
   }
--- main.js
+++ main.js
@@ -1,30 +1,57 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width = 200;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+const N = 100;
+const cars = generateCars(N);
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 animate();
+
+function generateCars(N) {
+  const cars = [];
+  for (let i = 1; i < N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
+  }
+  return cars;
+}
 
 function animate() {
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
   }
-  car.update(road.borders, traffic);
 
-  canvas.height = window.innerHeight;
-
-  ctx.save();
-  ctx.translate(0, -car.y + canvas.height * 0.7);
-
-  road.draw(ctx);
-  for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(ctx, "green");
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].update(road.borders, traffic);
   }
-  car.draw(ctx, "blue");
 
-  ctx.restore();
+  const bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
+
+  carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
+
+  carCtx.save();
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
+
+  road.draw(carCtx);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(carCtx, "green");
+  }
+  carCtx.globalAlpha = 0.2;
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].draw(carCtx, "blue");
+  }
+  carCtx.globalAlpha = 1;
+  bestCar.draw(carCtx, "blue", true);
+
+  carCtx.restore();
+
+  //Visualizer.drawNetwork(networkCtx, bestCar.brain);
   requestAnimationFrame(animate);
 }
