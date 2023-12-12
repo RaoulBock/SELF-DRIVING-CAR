1a7af3a847488d59e1fdbce5486073001eaf33b3
Raoul Bock <52930905+RaoulBock@users.noreply.github.com>
2023-12-12 22:35:53 +0200

    Update sensor.js

--- sensor.js
+++ sensor.js
@@ -2,7 +2,7 @@ class Sensor {
   constructor(car) {
     this.car = car;
     this.rayCount = 8;
-    this.rayLength = 140;
+    this.rayLength = 180;
     this.raySpread = Math.PI / 2.5;
 
     this.rays = [];
