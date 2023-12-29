3d8d5211499814b32e8609e3d457224d1a8b6ddf
RAOUL BOCK <52930905+RaoulBock@users.noreply.github.com>
2023-12-29 08:31:06 +0200

    Update car.js

--- car.js
+++ car.js
@@ -6,7 +6,7 @@ class Car {
     this.height = height;
 
     this.speed = 0;
-    this.acceleration = 0.1;
+    this.acceleration = 0.9;
     this.maxSpeed = maxSpeed;
     this.friction = 0.05;
     this.angle = 0;
